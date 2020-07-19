66ab4a2a12462bb5497819b0f477465591b34c3c
sebastianpopa <sebastian.code0@gmail.com>
2020-07-19 22:55:05 +0300

    Until 2.18

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,18 @@ import { Component } from '@angular/core';
 })
 export class ServersComponent {
   allowNewServer = false;
-
+  serverCreated = "No server has been created";
+  serverName = '';
   constructor() {
     setTimeout( () => {
       this.allowNewServer = true;
     },5000)
+  }
+  onServerCreation(){
+    this.serverCreated = "A server was created !!!";
+  }
+
+  onUpdateServerName(event: any){
+    this.serverName = event.target.value;
   }
 }
